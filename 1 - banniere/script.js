fbdbd8a53420c2357b9231c26f2f2636046921db
WakaTP-B <courcelles.amaury@orange.fr>
2025-09-26 16:31:51 +0200

    1 et 2 finish

--- 1 - banniere/script.js
+++ 1 - banniere/script.js
@@ -1,11 +1,19 @@
 /*
 Nouvelles compétences:
 - querySelector // Selectionne le premier element seulement mieux que getElementsBy
-- HTMLElement.style 
+- HTMLElement.style
 */
 
 // 1. Je recupere le bouton "Accepter les cookies"
+const acceptBtnElem = document.querySelector(".btn-accept");
 
 // 2. Je recupere la banniere de cookies
+const banniereCookieElem = document.querySelector(".cookies");
 
 // 3. J'ecoute l'evenement "click" sur le bouton "Accepter les cookies"
+acceptBtnElem.addEventListener("click", function () {
+    banniereCookieElem.classList.add("hide")
+    setTimeout(function () {
+        banniereCookieElem.remove();
+    }, 1000);
+});
--- 2 - abonne/script.js
+++ 2 - abonne/script.js
@@ -11,11 +11,32 @@ Nouvelles compétences:
 */
 
 // 1. Je recupere l'icone smiley
+const smileyElem = document.querySelector("i");
+//const smileyElem = document.querySelector("#emoji");
 
 // 2. Je recupere le bouton ABONNER
+const btnSubElem = document.querySelector(".btn-sub");
 
 // 3. Je défini une variable binaire qui exprime l'etat abonné ou non de l'utilisateur
+let isAboBol = false;
 
 // 4. J'ecoute le clique sur l'icone smiley
+smileyElem.addEventListener("click", function () {
+    smileyElem.classList.toggle("fa-smile-wink");
+    smileyElem.classList.toggle("happy");
+    smileyElem.classList.toggle("fa-face-meh");
+
+})
 
 // 5. J'ecoute l'evenemment click sur le bouton ABONNEZ
+btnSubElem.addEventListener("click", function () {
+    if (isAboBol === false) {
+        btnSubElem.classList.toggle("abonne");
+        btnSubElem.innerText = "Abonné";
+        isAboBol = true;
+    }else{
+        btnSubElem.classList.toggle("abonne");
+        btnSubElem.innerText = "Abonnez-vous";
+        isAboBol = false
+    }
+})
