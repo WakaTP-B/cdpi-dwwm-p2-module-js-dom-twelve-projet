6aba99db769cff96bc7e28d5a027cbc8c8f07846
WakaTP-B <courcelles.amaury@orange.fr>
2025-09-28 17:19:18 +0200

    darkmode

--- 3 - dark mode/script.js
+++ 3 - dark mode/script.js
@@ -3,3 +3,13 @@ Nouvelles compétences:
 - forEach
 - querySelectorAll
 */
+const allElemArr = document.querySelectorAll("*");
+const switchElem = document.querySelector(".switch");
+const iconElem = document.querySelector(".icon");
+
+switchElem.addEventListener("click", function () {
+    allElemArr.forEach(function (allElem) {
+        allElem.classList.toggle("darkmode");
+    })
+    iconElem.classList.toggle("fa-moon");
+})
